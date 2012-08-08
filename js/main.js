abcfa17110144dd57828dddf3e3448eeab4ec087
Michael Collis <mikec@whoisstudio.com>
2012-08-08 18:17:54 -0400

    adding updates for commute costs.

--- js/main.js
+++ js/main.js
@@ -7,46 +7,47 @@ $( function( ) {
         value: deflt,
         min: parseInt( params[0] ),
         max: parseInt( params[1] ),
-        step: parseInt( params[2] ),
+        step: parseFloat( params[2] ),
         slide: function( event, ui ) {
           $( this ).prev( '.default' ).find( '.amount' ).html( ui.value );
           calculate();
         }
       } );
     } );
+    $( '#car-size-select' ).change( function() {
+      data.size = parseFloat( $( '#car-size-select' ).val() );
+      calculate();
+    } );
+    data.size = parseFloat( $( '#car-size-select' ).val() );
     calculate();
 } );
 var data = {};
-function calculateFixed( ) {
-  data.purchase = parseInt( $( '#purchase .default .amount' ).html() );
-  data.duration = parseInt( $( '#duration .default .amount' ).html() );
-  data.resale = parseInt( $( '#resale .default .amount' ).html() );
-  data.insurance = parseInt( $( '#insurance .default .amount' ).html() );
-  data.registration = parseInt( $( '#registration .default .amount' ).html() );
-  data.inspection = parseInt( $( '#inspection .default .amount' ).html() );
-
-  data.fixedTotal = ( ( data.purchase - data.resale ) / ( data.duration * 365 ) ) + ( ( data.insurance * 12 ) / 365 ) + ( ( data.registration / 2 ) / 365 ) + ( data.inspection / 365 );
-  $( '#fixed-total .default .amount' ).html( data.fixedTotal.toFixed( 2 ) );
-}
-function calculateOperating( ) {
-  data.gas = parseInt( $( '#gas .default .amount' ).html() );
+function calculateCosts( ) {
   data.parking = parseInt( $( '#parking .default .amount' ).html() );
   data.tolls = parseInt( $( '#tolls .default .amount' ).html() );
-  data.tires = parseInt( $( '#tires .default .amount' ).html() );
-  data.repairs = parseInt( $( '#repairs .default .amount' ).html() );
-  data.maintenance = parseInt( $( '#maintenance .default .amount' ).html() );
-  data.tickets = parseInt( $( '#tickets .default .amount' ).html() );
+  data.roundtrip = parseInt( $( '#round-trip .default .amount' ).html() );
+  data.workdays = parseInt( $( '#work-days .default .amount' ).html() );
+  data.economy = parseInt( $( '#economy .default .amount' ).html() );
+  data.gas = parseFloat( $( '#gas-price .default .amount' ).html() );
+  $( '#gas-price .default .amount' ).html( data.gas.toFixed( 2 ) );
 
-  data.operatingTotal = ( ( data.gas * 52 ) / 365 ) + ( ( data.parking * 12 ) / 365 ) + ( ( data.tolls * 12 ) / 365 ) + ( data.tires / 365 ) + ( data.repairs / 365 ) + ( data.maintenance / 365 ) + ( data.tickets / 365 );
-  $( '#operating-total .default .amount' ).html( data.operatingTotal.toFixed( 2 ) );
+  data.totalMiles = data.roundtrip * data.workdays;
+  data.totalGallons = data.totalMiles / data.economy;
+  data.totalGas = data.totalGallons * data.gas;
+  data.totalMaintenance = data.totalMiles * data.size;
 }
-function calculate( ) {
-  calculateFixed();
-  calculateOperating();
 
-  var perDay = data.fixedTotal + data.operatingTotal;
-  var perMonth = perDay * 30;
-  var perYear = perDay * 365;
+function calculate( ) {
+  calculateCosts();
+
+  $( '#total-miles .default .amount' ).html( data.totalMiles );
+  $( '#total-gallons .default .amount' ).html( data.totalGallons.toFixed( 1 ) );
+  $( '#total-gas .default .amount' ).html( data.totalGas.toFixed( 2 ) );
+  $( '#total-maintenance .default .amount' ).html( data.totalMaintenance.toFixed( 2 ) );
+
+  var perMonth = data.parking + data.tolls + data.totalGas + data.totalMaintenance;
+  var perDay = perMonth / data.workdays;
+  var perYear = perMonth * 12;
 
   $( '#per-day-total .default .amount' ).html( perDay.toFixed( 2 ) );
   $( '#per-month-total .default .amount' ).html( perMonth.toFixed( 2 ) );
